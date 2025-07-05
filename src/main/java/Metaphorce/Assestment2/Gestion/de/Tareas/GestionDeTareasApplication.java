7550e7777903945e721b01ebdc859370c1def288
Yun <160649184+TheGodKillerYun@users.noreply.github.com>
2025-07-05 10:28:22 -0600

    Spring Boot
    Estructura Inicial

---
+++ src/main/java/Metaphorce/Assestment2/Gestion/de/Tareas/GestionDeTareasApplication.java
@@ -0,0 +1,13 @@
+package Metaphorce.Assestment2.Gestion.de.Tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeTareasApplication.class, args);
+	}
+
+}
