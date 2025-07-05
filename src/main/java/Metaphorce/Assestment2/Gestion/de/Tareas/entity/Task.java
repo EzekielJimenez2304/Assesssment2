cb4912dff509a8a77471e297b7de9c9fd21ef176
SEKI2304 <205829531+EzekielJimenez2304@users.noreply.github.com>
2025-07-05 11:11:19 -0600

    Creation entity flije

---
+++ src/main/java/Metaphorce/Assestment2/Gestion/de/Tareas/entity/Task.java
@@ -0,0 +1,47 @@
+package Metaphorce.Assestment2.Gestion.de.Tareas.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private boolean completed;
+
+    //Constructor vacio
+    public Task() {
+    }
+    public Task(int id, String title, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    //Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
